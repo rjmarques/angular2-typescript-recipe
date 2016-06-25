4e9426d6e58fece99f901d0313c34a86fa350acb
rjmarques <ricardomarques377@gmail.com>
2016-06-25 11:49:12 -0400

    using Webpack isntead of SystemJS for module loading and bundle creation

---
+++ server.js
@@ -0,0 +1,60 @@
+var port = 1337;
+var serverUrl = "0.0.0.0";
+
+var http = require("http");
+var path = require("path"); 
+var fs = require("fs"); 		
+
+console.log("Starting web server at " + serverUrl + ":" + port);
+
+http.createServer( function(req, res) {
+
+	var now = new Date();
+
+	var filename = req.url || "index.html";
+	var ext = path.extname(filename);
+	var localPath = __dirname + "/dist";
+	var validExtensions = {
+		".html" : "text/html",			
+		".js": "application/javascript", 
+		".css": "text/css",
+		".txt": "text/plain",
+		".jpg": "image/jpeg",
+		".gif": "image/gif",
+		".png": "image/png"
+	};
+	var isValidExt = validExtensions[ext];
+
+	if (isValidExt) {
+		
+		localPath += filename;
+		fs.exists(localPath, function(exists) {
+			if(exists) {
+				console.log("Serving file: " + localPath);
+				getFile(localPath, res, isValidExt);
+			} else {
+				console.log("File not found: " + localPath);
+				res.writeHead(404);
+				res.end();
+			}
+		});
+
+	} else {
+		console.log("Invalid file extension detected: " + ext)
+	}
+
+}).listen(port, serverUrl);
+
+function getFile(localPath, res, mimeType) {
+	fs.readFile(localPath, function(err, contents) {
+		if(!err) {
+			res.setHeader("Content-Length", contents.length);
+			res.setHeader("Content-Type", mimeType);
+			res.statusCode = 200;
+			res.end(contents);
+		} else {
+			res.writeHead(500);
+			res.end();
+		}
+	});
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import '../../public/css/styles.css';
+
+@Component({
+  selector: 'recipe-app',
+  template: require('./app.component.html'),
+  styles: [require('./app.component.css')]
+})
+
+export class AppComponent { }
