5780666a3064b0683ead1b69291272b21b8f3f3f
Ricardo Marques <ricardomarques377@gmail.com>
2016-12-28 21:47:57 +0000

    small refactor of unit test template

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,23 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import { SharedModule } from './shared';
+
 import { AppComponent } from './app.component';
-import { AppHeaderComponent } from './shared/header';
+
+import { PuppiesComponent } from './puppies';
+import { OttersComponent } from './otters';
+import { KittensComponent } from './kittens';
 
 describe('App', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent,
-        AppHeaderComponent
-      ],
-      imports: [
-	      RouterTestingModule
-      ]      
-    });
-  });
-  it ('should work', () => {
-    let fixture = TestBed.createComponent(AppComponent);
-    expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({			
+			imports: [
+				RouterTestingModule,
+				SharedModule
+			],
+			declarations: [
+				AppComponent,
+				PuppiesComponent,
+				OttersComponent,
+				KittensComponent
+			]
+		});
+	});
+	it ('should work', () => {
+		let fixture = TestBed.createComponent(AppComponent);
+		expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
+	});
 });
