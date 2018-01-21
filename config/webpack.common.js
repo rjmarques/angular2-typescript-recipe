bcf6d6b39f062144bacf9e92d3e08fca3d0cdb86
Ricardo Marques <ricardomarques377@gmail.com>
2018-01-21 17:24:38 +0000

    angular and package dependency updates

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -59,12 +59,8 @@ module.exports = {
 	},
 
 	plugins: [
-		// Workaround for angular/angular#11580
 		new webpack.ContextReplacementPlugin(
-			// The (\\|\/) piece accounts for path separators in *nix and Windows
-			/angular(\\|\/)core(\\|\/)@angular/,
-			helpers.root('./src'), // location of your src
-			{} // a map of your routes
+			/\@angular(\\|\/)core(\\|\/)esm5/, helpers.root('src'), {}
 		),
 
 		new webpack.optimize.CommonsChunkPlugin({
